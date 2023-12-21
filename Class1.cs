4fa7a2871e0970f7dd799697b199e90db3a3b2af
olimov Amir <olimov.amir@mail.ru>
2023-12-21 20:24:08 +0300

    fix bag

---
+++ Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Class1
+    {
+        public void MaxSum() 
+        {
+            // Пример массива чисел
+            int[] numbers = { 50, 5, 8, 3, 7 };
+
+            // Используем метод Max для нахождения максимального числа
+            int maxNumber = numbers.Max();
+
+            // Выводим результат
+            Console.WriteLine($"Максимальное число в массиве: {maxNumber}");
+        }
+
+        public void MinSum()
+        {
+            int[] numbers = { 10, 25, 41, 5, 15 };
+            int minNumber = numbers.Min();
+            Console.WriteLine($"Минимальное число в массиве: {minNumber}");
+        }
+
+        public void Sort() 
+        {
+            // Пример массива чисел
+            int[] numbers = { 10, 5, 8, 3, 7 };
+
+            // Сортируем массив по возрастанию
+            Array.Sort(numbers);
+
+            // Выводим отсортированный массив
+            Console.WriteLine("Отсортированный массив по возрастанию:");
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine(); // Переходим на новую строку
+        
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,13 @@ class Program
         Encapsulation.Gun gun = new Encapsulation.Gun();
 
         gun.Shoot();
+
+        Encapsulation.Class1 class1 = new Encapsulation.Class1();
+
+        class1.MaxSum();
+        class1.MinSum();
+        class1.Sort();
+
      }
 
 }
